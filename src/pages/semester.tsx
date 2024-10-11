8491d182a67bf96c018237302375c914aa0172e8
tuanloc-1708 <97837536+tuanloc-1708@users.noreply.github.com>
2024-10-11 12:38:07 +0700

    update semester start & end

--- src/pages/semester.tsx
+++ src/pages/semester.tsx
@@ -1,21 +1,26 @@
-import React from 'react'
 import { DatePicker } from '@nextui-org/react'
-import { now, parseAbsoluteToLocal } from '@internationalized/date'
-
-export default function SetupTimeStamp() {
-  let [date, setDate] = React.useState(parseAbsoluteToLocal('2021-04-07T18:45:22Z'))
-
+import { CalendarDate, parseDate, getLocalTimeZone, today } from '@internationalized/date'
+export default function App() {
   return (
-    <div className='w-full max-w-xl flex flex-col items-start gap-4'>
-      <DatePicker className='max-w-md' granularity='second' label='Date and time' value={date} onChange={setDate} />
-      <DatePicker className='max-w-md' granularity='day' label='Date' value={date} onChange={setDate} />
-      <DatePicker className='max-w-md' granularity='second' label='Event date' />
-      <DatePicker
-        className='max-w-md'
-        granularity='second'
-        label='Event date'
-        placeholderValue={now('Asia/Ho_Chi_Minh')}
-      />
+    <div className='flex w-full flex-col gap-4'>
+      <div className='flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4'>
+        <DatePicker
+          label='Semester Start'
+          isRequired
+          placeholderValue={new CalendarDate(1995, 11, 6)}
+          labelPlacement='inside'
+          minValue={today(getLocalTimeZone()) as CalendarDate}
+          defaultValue={today(getLocalTimeZone()).subtract({ days: 0 }) as CalendarDate}
+        />
+        <DatePicker
+          label='Semester End'
+          isRequired
+          placeholderValue={new CalendarDate(1995, 11, 6)}
+          labelPlacement='inside'
+          minValue={today(getLocalTimeZone()) as CalendarDate}
+          defaultValue={today(getLocalTimeZone()).subtract({ days: 0 }) as CalendarDate}
+        />
+      </div>
     </div>
   )
 }
