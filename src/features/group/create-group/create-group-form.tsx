4a1c810c4d58017d74d7283765f75797e4ede610
tuanloc-1708 <97837536+tuanloc-1708@users.noreply.github.com>
2024-10-24 00:16:35 +0700

    hot fix debounce search 1000ms

--- src/features/group/create-group/create-group-form.tsx
+++ src/features/group/create-group/create-group-form.tsx
@@ -30,31 +30,48 @@ export default function CreateGroupForm() {
   const [allUsers, setAllUsers] = React.useState<SearchUserResult[]>([])
   const [removedUsers, setRemovedUsers] = React.useState<SearchUserResult[]>([])
   const maxUsers = 4
+  const searchTimeoutRef = React.useRef<NodeJS.Timeout | null>(null)
 
   const handleSearch = React.useCallback(
-    async (value: string) => {
+    (value: string) => {
       setFilterValue(value)
 
-      if (!value) {
+      if (value.trim() === '') {
         setAvailableUsers([])
-        setIsLoading(false)
         setHasSearched(false)
+        setIsLoading(false)
         return
       }
 
-      setHasSearched(true)
-      setIsLoading(true)
-
-      try {
-        const result = await searchUserService.searchUsers([1], true, value, false)
-        const filteredResult = result.filter((user) => !selectedUsers.some((u) => u.userID === user.userID))
-        setAvailableUsers(filteredResult)
-        setAllUsers(result)
-      } catch (error) {
-        console.error('Error searching users:', error)
-      } finally {
-        setIsLoading(false)
+      if (searchTimeoutRef.current) {
+        clearTimeout(searchTimeoutRef.current)
       }
+
+      setIsLoading(true)
+      setAvailableUsers([])
+
+      searchTimeoutRef.current = setTimeout(async () => {
+        if (!value) {
+          setAvailableUsers([])
+          setIsLoading(false)
+          setHasSearched(false)
+          return
+        }
+
+        setHasSearched(true)
+
+        try {
+          const result = await searchUserService.searchUsers([1], true, value, false)
+          const filteredResult = result.filter((user) => !selectedUsers.some((u) => u.userID === user.userID))
+
+          setAvailableUsers(filteredResult)
+          setAllUsers(result)
+        } catch (error) {
+          console.error('Error searching users:', error)
+        } finally {
+          setIsLoading(false)
+        }
+      }, 1000)
     },
     [selectedUsers]
   )
@@ -137,7 +154,7 @@ export default function CreateGroupForm() {
             )}
           </div>
           <Table
-            aria-label='Example table with search and loading state'
+            aria-label='Table with search and loading state'
             isHeaderSticky
             classNames={{
               wrapper: 'max-h-[400px]'
